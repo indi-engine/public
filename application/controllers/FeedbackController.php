691617f45f4212129307fef41b147e588dfbd0d4
Pavel Perminov <pavel.perminov.23@gmail.com>
2014-05-29 23:01:42 +0300

    remaining $this->post replased with Indi::post()

--- application/controllers/FeedbackController.php
+++ application/controllers/FeedbackController.php
@@ -5,7 +5,7 @@ class FeedbackController extends Project_Controller_Front{
 		if (Indi::post()->captcha != $_SESSION['captcha']['feedback']) {
 			$json['errors'][] = 'Вы ввели неправильный проверочный код';
 		} else {
-			$data = $this->post;
+			$data = Indi::post();
 			$data['date'] = date('Y-m-d');
 			Indi::model('Feedback')->createRow($data)->save();
 			$json['ok'] = 'Ваше сообщение отправлено';

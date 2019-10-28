2438c988d221e17509a436e0e53ee876210fbfcf
Pavel Perminov <pavel.perminov.23@gmail.com>
2019-10-28 22:58:25 +0300

    protocol-relative vk-urls

--- library/Indi/View/Helper/InitVkontakte.php
+++ library/Indi/View/Helper/InitVkontakte.php
@@ -8,8 +8,8 @@ class Indi_View_Helper_InitVkontakte {
 
         // Include the vkontakte javascipt SDK
         ob_start();?>
-<script async src="http://vkontakte.ru/js/api/openapi.js" type="text/javascript" charset="windows-1251"></script>
-<script type="text/javascript" src="http://userapi.com/js/api/openapi.js?45"></script>
+<script async src="//vkontakte.ru/js/api/openapi.js" type="text/javascript" charset="windows-1251"></script>
+<script type="text/javascript" src="//userapi.com/js/api/openapi.js?45"></script>
 <script type="text/javascript">VK.init({apiId: <?=$apiId?>});</script>
 <div id="vk_auth"></div>
 		<?$xhtml = ob_get_clean();
